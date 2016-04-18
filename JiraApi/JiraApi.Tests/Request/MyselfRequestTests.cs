c12dfe305597d3ea0447a5fbda7e1d69b18842eb
RomaRudayk <roma.rudyak@gmail.com>
2016-04-19 01:09:17 +0300

    Add tests
    + MyselfRequestTests
    + ProjectRequestTests

---
+++ JiraApi/JiraApi.Tests/Request/MyselfRequestTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiraApi.Request;
+using System.Net.Http;
+
+namespace JiraApi.Tests.Request
+{
+    [TestClass]
+    public class MyselfRequestTests
+    {
+        [TestMethod]
+        public void MyselfApiPathTest()
+        {
+            var expectedPath = "/rest/api/2/myself";
+
+            var request = new MyselfRequest(HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+    }
+}
---
+++ JiraApi/JiraApi.Tests/Request/ProjectRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiraApi.Request;
+using System.Net.Http;
+
+namespace JiraApi.Tests.Request
+{
+    [TestClass]
+    public class ProjectRequestTests
+    {
+        [TestMethod]
+        public void ProjectApiPathTest()
+        {
+            var expectedPath = "/rest/api/2/project";
+
+            var request = new ProjectRequest(HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+
+        [TestMethod]
+        public void ProjectApiPathWithKeyOrIdTest()
+        {
+            var expectedPath = "/rest/api/2/project/TESTPROJECT";
+            var keyOrId = "TESTPROJECT";
+
+            var request = new ProjectRequest(keyOrId, HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+    }
+}
