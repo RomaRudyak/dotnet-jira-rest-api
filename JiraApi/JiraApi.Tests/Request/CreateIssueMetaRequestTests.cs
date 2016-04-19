2865bf5245fb61c61f7140ea3494a405af4ed4b4
RomaRudayk <roma.rudyak@gmail.com>
2016-04-19 21:56:02 +0300

    Add Tests
    + PriorityRequestTests
    + CreateIssueMetaRequestTests

---
+++ JiraApi/JiraApi.Tests/Request/CreateIssueMetaRequestTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiraApi.Request;
+using System.Net.Http;
+
+namespace JiraApi.Tests.Request
+{
+    [TestClass]
+    public class CreateIssueMetaRequestTests
+    {
+        [TestMethod]
+        public void CreateIssueMetaApiPathTest()
+        {
+            var expectedPath = "/rest/api/2/issue/createmeta";
+
+            var request = new CreateIssueMetaRequest(HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+
+        [TestMethod]
+        public void CreateIssueMetaApiPathWithProjectIdsParamTest()
+        {
+            var expectedPath = "/rest/api/2/issue/createmeta?projectIds=12345,54321";
+            var ids = new[] { "12345", "54321" };
+
+            var request = new CreateIssueMetaRequest(HttpMethod.Get)
+            {
+                ProjectIds = ids
+            };
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+
+        [TestMethod]
+        public void CreateIssueMetaApiPathWithProjectKeysParamTest()
+        {
+            var expectedPath = "/rest/api/2/issue/createmeta?projectKeys=TESTPROJECT1,TESTPROJECT2";
+            var keys = new[] { "TESTPROJECT1", "TESTPROJECT2" };
+
+            var request = new CreateIssueMetaRequest(HttpMethod.Get)
+            {
+                ProjectKeys = keys
+            };
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+
+        [TestMethod]
+        public void CreateIssueMetaApiPathWithProjectIdsAndProjectKeysParamTest()
+        {
+            var expectedPath = "/rest/api/2/issue/createmeta?projectIds=12345,54321&projectKeys=TESTPROJECT1,TESTPROJECT2";
+            var ids = new[] { "12345", "54321" };
+            var keys = new[] { "TESTPROJECT1", "TESTPROJECT2" };
+
+            var request = new CreateIssueMetaRequest(HttpMethod.Get)
+            {
+                ProjectIds = ids,
+                ProjectKeys = keys
+            };
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+    }
+}
---
+++ JiraApi/JiraApi.Tests/Request/PriorityRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiraApi.Request;
+using System.Net.Http;
+
+namespace JiraApi.Tests.Request
+{
+    [TestClass]
+    public class PriorityRequestTests
+    {
+        [TestMethod]
+        public void PriorityApiPathTest()
+        {
+            var expectedPath = "/rest/api/2/priority";
+
+            var request = new PriorityRequest(HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+
+        [TestMethod]
+        public void ProjectApiPathWithKeyOrIdTest()
+        {
+            var expectedPath = "/rest/api/2/priority/TESTPRIORITY";
+            var keyOrId = "TESTPRIORITY";
+
+            var request = new PriorityRequest(keyOrId, HttpMethod.Get);
+
+            var path = request.BuildPath();
+
+            Assert.AreEqual(expectedPath, path);
+        }
+    }
+}
