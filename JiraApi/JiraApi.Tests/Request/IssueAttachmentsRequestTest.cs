396a1cfbe77ce90f86d3057a1a4671b3bbfd62ce
RomaRudayk <roma.rudyak@gmail.com>
2016-04-19 23:12:22 +0300

    Add Test for IssueAttachmentsRequestTest

---
+++ JiraApi/JiraApi.Tests/Request/IssueAttachmentsRequestTest.cs
@@ -0,0 +1,26 @@
+﻿using JiraApi.Request;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraApi.Tests.Request
+{
+    [TestClass]
+    public class IssueAttachmentsRequestTest
+    {
+        [TestMethod]
+        public void IssueAttachmentsApiPathWithKeyOrIdTest()
+        {
+            var expectedPath = "/rest/api/2/issue/TEST-1324/attachments";
+            var key = "TEST-1324";
+
+            var request = new IssueAttachmentsRequest(key, HttpMethod.Get);
+
+            Assert.AreEqual(expectedPath, request.BuildPath());
+        }
+    }
+}
